4c70b6e7e1fa91f7bc34f095030eb138f277defd
Iryna Kovalova <kovra777@gmail.com>
2024-02-29 23:27:47 +0200

    Modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import searchImages from '../api';
-
 import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -9,7 +9,7 @@ const modalStyle = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     backgroundColor: 'transparent',
-    height: '50%',
+    height: '100%',
     border: 'none',
   },
 };
