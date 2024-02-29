6fccff16c8a6ea65f919eef5cd66731efcbaff8c
Iryna Kovalova <kovra777@gmail.com>
2024-02-29 21:26:09 +0200

    Update Modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,4 +1,31 @@
-const ImageModal = ({ children }) => {
-    return <>{children}</>;
-  };
-  export default ImageModal;
+import css from './ImageModal.module.css';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
+const modalStyle = {
+  overlay: { backgroundColor: 'rgba(0, 0, 0, 0.7)' },
+  content: {
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'transparent',
+    height: '100%',
+    border: 'none',
+  },
+};
+
+const ImageModal = ({ onCloseModal, content }) => {
+  return (
+    <>
+      <ReactModal
+        isOpen={true}
+        onRequestClose={onCloseModal}
+        shouldCloseOnEsc={true}
+        style={modalStyle}
+      >
+        <img className={css.img} src={content.urls.regular} width={'100%'} height={'100%'} />
+      </ReactModal>
+    </>
+  );
+};
+export default ImageModal;
