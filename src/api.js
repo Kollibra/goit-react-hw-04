97108559ab1e15fca757751e3557d30688e8c7b9
Iryna Kovalova <kovra777@gmail.com>
2024-02-29 21:50:43 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,13 @@
+import axios from 'axios';
 
+const API_KEY = '3yTSMbvB6WVLwmn3CLFvDf8bU-8JkBsEFQgA50rh7qY';
+const searchImages = async (query, page) => {
+  const response = await axios.get(
+    `https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=${page}&per_page=12&query=${query}`
+  );
+  console.log(response.data);
+
+  return response.data;
+};
+export default searchImages;
 
