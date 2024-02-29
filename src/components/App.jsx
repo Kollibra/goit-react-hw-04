f21fa58c7e80fbe8078f608b7da690664784d2b6
Iryna Kovalova <kovra777@gmail.com>
2024-02-29 22:21:52 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,81 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
-import "./index.css";
+import { useState, useEffect } from 'react';
+import searchImages from '../api';
 
-ReactDOM.createRoot(document.getElementById('root')).render() 
-
-
+import SearchBar from './SearchBar/SearchBar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
+import Loader from './Loader/Loader';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
+import ImageModal from './ImageModal/ImageModal';
 
 const App = () => {
-  
+  const [images, setImages] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [searchValue, setSearchValue] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState({});
+  const [btnLoadMore, setBtnLoadMore] = useState(false);
+
+  const onSubmit = query => {
+    if (query === searchValue) {
+      return;
+    }
+    setSearchValue(query);
+    setImages([]);
+    setPage(1);
+  };
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const data = await searchImages(searchValue, page);
+        const { results, total_pages } = data;
+        setImages(prev => [...prev, ...results]);
+        setBtnLoadMore(total_pages && total_pages !== page);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoader(false);
+      }
+    };
+    if (searchValue === '') {
+      return;
+    }
+    setLoader(true);
+    fetchImages();
+  }, [page, searchValue]);
+
+  const onClick = () => {
+    setPage(page + 1);
+  };
+
+  const onOpenModal = content => {
+    setModalContent(content);
+    setShowModal(true);
+  };
+
+  const onCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
+      <SearchBar onSubmit={onSubmit} />
 
-       
+      {images.length > 0 && (
+        <ImageGallery items={images} onModalOpen={onOpenModal} />
+      )}
+      {error && <ErrorMessage />}
+
+      {loader && <Loader />}
+      {btnLoadMore && <LoadMoreBtn onClick={onClick} />}
+
+      {showModal && (
+        <ImageModal onCloseModal={onCloseModal} content={modalContent} />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
