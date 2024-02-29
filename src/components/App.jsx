877f58447040f8b763c1b5a9ad1417a75a019088
Iryna Kovalova <kovra777@gmail.com>
2024-02-29 22:15:56 +0200

    Update App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render() 
+
+
+
+function App() {
+  
+  return (
+    <>
+
+       
+    </>
+  )
+}
+
+export default App
